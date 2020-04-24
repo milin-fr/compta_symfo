3a8e663332c320858db61e226a04c633e2c8b04e
unknown <m.ilin.fr@gmail.com>
2020-04-24 11:15:55 +0200

    WorkType entity

--- src/Entity/WorkType.php
+++ src/Entity/WorkType.php
@@ -21,6 +21,31 @@ class WorkType
      */
     private $title;
 
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $budgetEuro;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $budgetCent;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updatedAt;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $enabled;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -34,6 +59,66 @@ class WorkType
     public function setTitle(string $title): self
     {
         $this->title = $title;
+
+        return $this;
+    }
+
+    public function getBudgetEuro(): ?int
+    {
+        return $this->budgetEuro;
+    }
+
+    public function setBudgetEuro(int $budgetEuro): self
+    {
+        $this->budgetEuro = $budgetEuro;
+
+        return $this;
+    }
+
+    public function getBudgetCent(): ?int
+    {
+        return $this->budgetCent;
+    }
+
+    public function setBudgetCent(int $budgetCent): self
+    {
+        $this->budgetCent = $budgetCent;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getEnabled(): ?bool
+    {
+        return $this->enabled;
+    }
+
+    public function setEnabled(bool $enabled): self
+    {
+        $this->enabled = $enabled;
 
         return $this;
     }
