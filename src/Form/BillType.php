f2c5b1ae715a3d75d9b804071113f2daf8cc8aca
unknown <m.ilin.fr@gmail.com>
2020-05-24 22:06:56 +0200

    bill crud

---
+++ src/Form/BillType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Bill;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BillType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('billNumber')
+            ->add('description')
+            ->add('priceEuro')
+            ->add('priceCent')
+            ->add('startDate')
+            ->add('endDate')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('enabled')
+            ->add('workType')
+            ->add('company')
+            ->add('status')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Bill::class,
+        ]);
+    }
+}
