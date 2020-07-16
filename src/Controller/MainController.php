00cde38080b2833cdb7be2184ed48c3efdf33ed5
unknown <m.ilin.fr@gmail.com>
2020-07-16 15:52:01 +0200

    added logic to display sum of expenses per company and per work type

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,6 +6,7 @@ use App\Entity\Company;
 use App\Form\CompanyType;
 use App\Repository\BillRepository;
 use App\Repository\CompanyRepository;
+use App\Repository\WorkTypeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,15 +20,43 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="home", methods={"GET"})
      */
-    public function home(CompanyRepository $companyRepository, BillRepository $billRepository): Response
+    public function home(CompanyRepository $companyRepository, WorkTypeRepository $workTypeRepository): Response
     {
-        $sumPerCompany = [];
+        $companies = [];
         foreach($companyRepository->findAll() as $company){
-            $company->sum = 123;
+            $euroSum = 0;
+            $centSum = 0;
+            foreach($company->getBills() as $bill){
+                if($bill->getStatus()->getDeductable() == 1){
+                    $euroSum += $bill->getPriceEuro();
+                    $centSum += $bill->getPriceCent();
+                }
+            }
+            $euroSum += intdiv($centSum, 100);
+            $centSum = $centSum % 100;
+            $company->euroSum = $euroSum;
+            $company->centSum = $centSum;
+            $companies[] = $company;
+        };
+
+        $workTypes = [];
+        foreach($workTypeRepository->findAll() as $workType){
+            $euroSum = 0;
+            $centSum = 0;
+            foreach($workType->getBills() as $bill){
+                $euroSum += $bill->getPriceEuro();
+                $centSum += $bill->getPriceCent();
+            }
+            $euroSum += intdiv($centSum, 100);
+            $centSum = $centSum % 100;
+            $workType->euroSum = $euroSum;
+            $workType->centSum = $centSum;
+            $workTypes[] = $workType;
         };
 
         return $this->render('main/home.html.twig', [
-            'companies' => $companyRepository->findAll(),
+            'companies' => $companies,
+            'workTypes' => $workTypes
         ]);
     }
 }
